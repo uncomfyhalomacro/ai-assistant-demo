d3d9bfff180158a106b1e685b29b45259fcf3d41
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-10 08:09:46 +0800

    add function to write to file
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

---
+++ functions/write_file.py
@@ -0,0 +1,25 @@
+import os
+import functools
+
+
+def write_file(working_directory, file_path, content):
+    working_directory = os.path.abspath(working_directory)
+    joined_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not joined_file_path.startswith(working_directory):
+        return f'Error: Cannot write "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(joined_file_path):
+        path_segments = joined_file_path.split("/")
+        _last_path_segment = path_segments.pop()
+        reconstructed_path_segments = "/" + functools.reduce(
+            os.path.join, path_segments
+        )
+        print(reconstructed_path_segments)
+        if reconstructed_path_segments == working_directory:
+            return f'Error: Cannot write "{file_path}" as it points to a directory.'
+        os.makedirs(reconstructed_path_segments, exist_ok=True)
+
+    with open(joined_file_path, "w") as f:
+        f.write(content)
+
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 class TestGetFileInfo(unittest.TestCase):
@@ -12,9 +13,9 @@ class TestGetFileInfo(unittest.TestCase):
         print(output)
         testcase = """Result for current directory:
  - main.py: file_size=565 bytes, is_dir=False
- - pkg: file_size=63 bytes, is_dir=True
+ - pkg: file_size=84 bytes, is_dir=True
  - tests.py: file_size=1331 bytes, is_dir=False
- - lorem.txt: file_size=20068 bytes, is_dir=False"""
+ - lorem.txt: file_size=28 bytes, is_dir=False"""
         self.assertEqual(output, testcase)
 
     def test_calculator_with_pkg(self):
@@ -23,7 +24,8 @@ class TestGetFileInfo(unittest.TestCase):
         testcase = """Result for 'pkg' directory:
  - calculator.py: file_size=1721 bytes, is_dir=False
  - render.py: file_size=754 bytes, is_dir=False
- - __pycache__: file_size=70 bytes, is_dir=True"""
+ - __pycache__: file_size=70 bytes, is_dir=True
+ - morelorem.txt: file_size=26 bytes, is_dir=False"""
         self.assertEqual(output, testcase)
 
     def test_calculator_with_slash_bin(self):
@@ -64,6 +66,30 @@ class TestGetFileContent(unittest.TestCase):
 
     def test_outside_working_dir(self):
         output = self.get_file_content("calculator", "/bin/cat")
+        print(output)
+
+
+# For now, we just print output
+class TestWriteFileContent(unittest.TestCase):
+    def setUp(self):
+        self.write_file = write_file
+
+    def test_write_to_lorem_in_calculator(self):
+        output = self.write_file(
+            "calculator", "lorem.txt", "wait, this isn't lorem ipsum"
+        )
+        print(output)
+
+    def test_morelorem_in_calculator(self):
+        output = self.write_file(
+            "calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"
+        )
+        print(output)
+
+    def test_fail_write_outside_workdir(self):
+        output = self.write_file(
+            "calculator", "/tmp/temp.txt", "this should not be allowed"
+        )
         print(output)
 
 
